3a18996ea4985b7c015954884beefd7d29c6906a
fgpsms <87663477+fgpsms@users.noreply.github.com>
2022-10-25 21:22:00 -0400

    Update index.tsx

--- src/components/GameBanner/index.tsx
+++ src/components/GameBanner/index.tsx
@@ -3,14 +3,19 @@
  * (2pt) - Use a interface como o tipo das props do componente e substitua os 
  *          atributos estáticos das imagem (src), titulo e contador de anuncios pelas respectivas props
  */
+interface GameBannerProps{
+  key: string,
+  image: string,
+  title: string,
+  contador: number;
+}
 
-export function GameBanner() {
-  return (<><a href="" className="relative rounded-lg overflow-hidden">
-    <img src="https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-188x250.jpg" alt="" />
-
+export function GameBanner(props: GameBannerProps) {
+  return (<><a href={props.key} className="relative rounded-lg overflow-hidden">
+    <img src={props.image} alt="" />
     <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-      <strong className="font-bold text-white block">GTA V</strong>
-      <span className="text-zinc-300 text-sm block">3 anuncio(s)</span>
+      <strong className="font-bold text-white block">{props.title}</strong>
+      <span className="text-zinc-300 text-sm block">{props.contador} anuncio(s)</span>
     </div>
   </a></>);
 }
